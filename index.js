ec4889e4a48150c47dbbdf56132e97a9da1f36be
icecreamsoldier <mariana.9000@hotmail.com>
2019-04-06 20:28:54 -0600

    Disabled keys

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 ////////////////////////////////////////////////////////////////////////////////
 /*global THREE, document, window  */
+
+'use strict';
+
+Physijs.scripts.worker = '/js/physijs_worker.js';
+Physijs.scripts.ammo = 'ammo.js';
+
 var camera, scene, renderer;
 var cameraControls;
+var ball, box;
+
+var friction = 0.4;
+var restitution = 0.6;
+var gravity = -50;
 
 var clock = new THREE.Clock();
+var keyboard = new KeyboardState();
+var rotSpeed = 0.01;
 
 function fillScene() {
-  scene = new THREE.Scene();
-
+  scene = new Physijs.Scene;
+  // scene.setGravity(new THREE.Vector3(0, gravity, 0));
   // LIGHTS
 
   scene.add(new THREE.AmbientLight(0x222222));
@@ -31,6 +44,46 @@ function fillScene() {
   axes.position.y = 1;
   scene.add(axes);
 
+  var ballGeometry = new THREE.SphereGeometry(50, 50, 50);
+  var ballMaterial = new THREE.MeshLambertMaterial({
+    color: 'yellow',
+    wireframe: false
+  });
+  // var ballMaterial = Physijs.createMaterial(
+  //   new THREE.MeshPhongMaterial({
+  //     color: 0xffffff,
+  //     transparent: false,
+  //     opacity: 0.1
+  //   }),
+  //   3.5,
+  //   0
+  // );
+  ball = new Physijs.BoxMesh(ballGeometry, ballMaterial, 0)
+  ball.setAngularFactor(THREE.Vector3(0,0,0));
+  // ball = new THREE.Mesh(ballGeometry, ballMaterial);
+  ball.position.x = 0;
+  ball.position.y = 30;
+  ball.position.z = 0;
+  scene.add(ball);
+
+
+  var boxGeometry = new THREE.BoxGeometry(100, 100, 100)
+  var transparentMaterial = Physijs.createMaterial(
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      transparent: false,
+      opacity: 0.1
+    }),
+    friction,
+    0
+  );
+  box = new Physijs.BoxMesh(boxGeometry, transparentMaterial, 0);
+
+  box.position.x = 200;
+  box.position.y = 30;
+  box.position.z = 0;
+  scene.add(box);
+
   drawElephant();
 }
 
@@ -49,7 +102,7 @@ function drawElephant() {
   };
   var onError = function(xhr) {};
 
-// Load The Room
+  // Load The Room
   var mtlLoader = new THREE.MTLLoader();
   mtlLoader.setPath('assets/');
   mtlLoader.load('Room.mtl', function(materials) {
@@ -58,10 +111,10 @@ function drawElephant() {
     objLoader.setMaterials(materials);
     objLoader.setPath('assets/');
     objLoader.load('Room.obj', function(object) {
-      object.position.y = 500;
-      object.scale.z = 30;
-      object.scale.x = 30;
-      object.scale.y = 30;
+      object.position.y = 0;
+      object.scale.z = 50;
+      object.scale.x = 50;
+      object.scale.y = 50;
       scene.add(object);
     }, onProgress, onError);
   });
@@ -70,8 +123,10 @@ function drawElephant() {
 }
 
 function init() {
+  // var canvasWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  // var canvasHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
   var canvasWidth = 600;
-  var canvasHeight = 400;
+  var canvasHeight = 600;
   var canvasRatio = canvasWidth / canvasHeight;
 
   // RENDERER
@@ -84,13 +139,56 @@ function init() {
   renderer.setClearColor(0xAAAAAA, 1.0);
 
   // CAMERA
-  camera = new THREE.PerspectiveCamera(10, canvasRatio, 7000, 18000);
-  camera.position.set(-7000, 4500, 7000);
+  camera = new THREE.PerspectiveCamera(45, canvasRatio, 0.1, 10000);
+  camera.position.set(0, -10, 0);
 
   // CONTROLS
-  cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
+  cameraControls = new THREE.OrbitControls(camera);
+  cameraControls.keys = {
+    LEFT: 4, //left arrow
+	  UP: 3, // up arrow
+	  RIGHT: 2, // right arrow
+	  BOTTOM: 1 // down arrow
+  }
   cameraControls.target.set(0, 500, 0);
+
 }
+
+
+function keyInput() {
+  var x = camera.position.x,
+    y = camera.position.y,
+    z = camera.position.z;
+
+  if (keyboard.pressed("A")) {
+    camera.position.x = x * Math.cos(rotSpeed) - z * Math.sin(rotSpeed);
+    camera.position.z = z * Math.cos(rotSpeed) + x * Math.sin(rotSpeed);
+  }
+  if (keyboard.pressed("D")) {
+    camera.position.x = x * Math.cos(rotSpeed) + z * Math.sin(rotSpeed);
+    camera.position.z = z * Math.cos(rotSpeed) - x * Math.sin(rotSpeed);
+  }
+  if (keyboard.pressed("down")) {
+    ball.translateZ(5);
+  }
+
+  // if (keyboard.pressed("up")) {
+  //   // ball.position.z = ball.position.z - 5;
+  //   ball.setLinearVelocity({z: -10, y:0,x:0});
+  // }
+
+  if (keyboard.pressed("left")) {
+    ball.translateX(-5);
+  }
+
+  if (keyboard.pressed("right")) {
+    ball.translateX(5);
+  }
+
+
+  camera.lookAt(scene.position);
+}
+
 
 function addToDOM() {
   var canvas = document.getElementById('canvas');
@@ -99,7 +197,9 @@ function addToDOM() {
 }
 
 function animate() {
+  keyboard.update();
   window.requestAnimationFrame(animate);
+  keyInput();
   render();
 }
 
@@ -110,7 +210,8 @@ function render() {
   const rotationStep = Math.PI / 180;
   // camera.position.applyAxisAngle(yAxis, xAxis);
 
-  cameraControls.update();
+  cameraControls.update(delta);
+  scene.simulate();
   renderer.render(scene, camera);
 }
 
